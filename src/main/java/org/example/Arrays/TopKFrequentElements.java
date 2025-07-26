204025a8b723030f938c1149ba00e51c3073745c
Maynor David <134113042+Maynor06@users.noreply.github.com>
2025-07-26 17:12:10 -0600

    iniciando repo de ejercicios

---
+++ src/main/java/org/example/Arrays/TopKFrequentElements.java
@@ -0,0 +1,14 @@
+package org.example.Arrays;
+
+public class TopKFrequentElements {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[nums.length];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
